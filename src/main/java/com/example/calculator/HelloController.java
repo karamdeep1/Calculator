d70db984430065ef29f306bb57b7e55b9409a6a3
Karamdeep <ricardo036942@gmail.com>
2023-04-22 19:49:58 -0400

    Design finished with some css.

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,14 +1,10 @@
 package com.example.calculator;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import java.lang.Math;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
