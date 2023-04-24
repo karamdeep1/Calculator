fc7c4fd517ebc47491955bc7aa49e6825783c4d5
Karamdeep <ricardo036942@gmail.com>
2023-04-23 23:48:02 -0400

    Fixed the equalsBoard method and added in the rest of operations. Only thing that needs fixing is putting the numbers on the board and getting rid of the zero that comes when clicking a number.

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.lang.Math;
+import java.util.Stack;
 
 public class HelloController extends HelloApplication{
     public HelloController() {
@@ -75,12 +76,9 @@ public class HelloController extends HelloApplication{
     private Button percentageButton;
     String expression = "";
     public void putNumOnBoard(ActionEvent e) throws IOException {
-        EventHandler<MouseEvent> buttonClickHandler = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event){
-                expression += ((Button)event.getSource()).getText();
-                calcBoardLabel.setText(expression);
-            }
+        EventHandler<MouseEvent> buttonClickHandler = event -> {
+            expression += ((Button)event.getSource()).getText();
+            calcBoardLabel.setText(expression);
         };
         for(Node node : anchorPane.getChildren()){
             if(node instanceof Button)
@@ -88,55 +86,167 @@ public class HelloController extends HelloApplication{
         }
     }
     public void clearBoard(ActionEvent e) throws IOException {
-        clearButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
-            public void handle(MouseEvent event){
-                expression ="";
-                calcBoardLabel.setText("0");
-            }
+        clearButton.setOnMouseClicked(event -> {
+            expression ="";
+            calcBoardLabel.setText("0");
         });
     }
     public void percentBoard(ActionEvent e) throws IOException {
-        percentageButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
-            public void handle(MouseEvent event){
+        if(calcBoardLabel.getText().equals("Error")){
+            calcBoardLabel.setText("0");
+        }
+        else {
+            percentageButton.setOnMouseClicked(event -> {
                 double percent = Double.parseDouble(calcBoardLabel.getText()) / 100;
                 calcBoardLabel.setText(String.valueOf(percent));
-            }
-        });
+            });
+        }
     }
     public void operationsBoard(ActionEvent e) throws IOException {
-        additionButton.setOnMouseClicked(event -> {
-            calcBoardLabel.setText(calcBoardLabel.getText() + " " + "+" + " ");
-            expression = calcBoardLabel.getText();
-        });
-        subtractButton.setOnMouseClicked(event -> {
-            calcBoardLabel.setText(calcBoardLabel.getText() + " " + "-" + " ");
-            expression = calcBoardLabel.getText();
-        });
-        multiplyButton.setOnMouseClicked(event -> {
-            calcBoardLabel.setText(calcBoardLabel.getText() + " " + "*" + " ");
-            expression = calcBoardLabel.getText();
-        });
-        divideButton.setOnMouseClicked(event -> {
-            calcBoardLabel.setText(calcBoardLabel.getText() + " " + "/" + " ");
-            expression = calcBoardLabel.getText();
-        });
-        numberSignButton.setOnMouseClicked(event -> {
-            if(calcBoardLabel.getText().charAt(0) == '-') {
-                calcBoardLabel.setText(calcBoardLabel.getText().substring(1));
-                expression = calcBoardLabel.getText();
-            } else{
-                calcBoardLabel.setText("-" + calcBoardLabel.getText());
-                expression = calcBoardLabel.getText();
-            }
-        });
-    }
-    public void equalsBoard(ActionEvent e){
-        double total = 0;
-        for (int i = 0; i < calcBoardLabel.getText().length(); i++) {
-            if(Character.isDigit(calcBoardLabel.getText().charAt(i)))
-                total+=Character.getNumericValue(calcBoardLabel.getText().charAt(i));
+        if(calcBoardLabel.getText().equals("Error")){
+            calcBoardLabel.setText("0");
         }
-        calcBoardLabel.setText(String.valueOf(total));
-        expression = String.valueOf(total);
+        else {
+            additionButton.setOnMouseClicked(event -> {
+                if (!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().substring(1).contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))) {
+                    calcBoardLabel.setText(calcBoardLabel.getText() + " " + "+" + " ");
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            subtractButton.setOnMouseClicked(event -> {
+                if (!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().substring(1).contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))) {
+                    calcBoardLabel.setText(calcBoardLabel.getText() + " " + "-" + " ");
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            multiplyButton.setOnMouseClicked(event -> {
+                if (!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().substring(1).contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))) {
+                    calcBoardLabel.setText(calcBoardLabel.getText() + " " + "*" + " ");
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            divideButton.setOnMouseClicked(event -> {
+                if (!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().substring(1).contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))) {
+                    calcBoardLabel.setText(calcBoardLabel.getText() + " " + "/" + " ");
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            numberSignButton.setOnMouseClicked(event -> {
+                if (calcBoardLabel.getText().charAt(0) == '-') {
+                    calcBoardLabel.setText(calcBoardLabel.getText().substring(1));
+                    expression = calcBoardLabel.getText();
+                } else {
+                    calcBoardLabel.setText("-" + calcBoardLabel.getText());
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            squareRootButton.setOnMouseClicked(event ->{
+                if(!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))){
+                    double num = Double.parseDouble(calcBoardLabel.getText());
+                    num = Math.sqrt(num);
+                    calcBoardLabel.setText(String.valueOf(num));
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            squaredButton.setOnMouseClicked(event ->{
+                if(!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))){
+                    double num = Double.parseDouble(calcBoardLabel.getText());
+                    num = Math.pow(num,2);
+                    calcBoardLabel.setText(String.valueOf(num));
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            sinButton.setOnMouseClicked(event ->{
+                if(!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))){
+                    double num = Double.parseDouble(calcBoardLabel.getText());
+                    num = Math.sin(num);
+                    calcBoardLabel.setText(String.valueOf(num));
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            cosButton.setOnMouseClicked(event ->{
+                if(!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))){
+                    double num = Double.parseDouble(calcBoardLabel.getText());
+                    num = Math.cos(num);
+                    calcBoardLabel.setText(String.valueOf(num));
+                    expression = calcBoardLabel.getText();
+                }
+            });
+            tanButton.setOnMouseClicked(event ->{
+                if(!(calcBoardLabel.getText().contains("+") || calcBoardLabel.getText().contains("-") || calcBoardLabel.getText().contains("*") || calcBoardLabel.getText().contains("/"))){
+                    double num = Double.parseDouble(calcBoardLabel.getText());
+                    num = Math.tan(num);
+                    calcBoardLabel.setText(String.valueOf(num));
+                    expression = calcBoardLabel.getText();
+                }
+            });
+        }
+    }
+
+    public void equalsBoard(ActionEvent e) {
+        if(calcBoardLabel.getText().equals("Error")){
+            calcBoardLabel.setText("0");
+        }
+        else {
+            String input = calcBoardLabel.getText();
+            String[] tokens = input.split(" ");
+            Stack<Double> numbers = new Stack<>();
+            Stack<String> operators = new Stack<>();
+            for (String token : tokens) {
+                if (token.matches("[+\\-*/]")) {
+                    while (!operators.isEmpty() && hasPrecedence(token, operators.peek())) {
+                        double second = numbers.pop();
+                        double first = numbers.pop();
+                        String op = operators.pop();
+                        numbers.push(applyOp(first, second, op));
+                    }
+                    operators.push(token);
+                } else {
+                    numbers.push(Double.parseDouble(token));
+                }
+            }
+            while (!operators.isEmpty()) {
+                double second = numbers.pop();
+                double first = numbers.pop();
+                String op = operators.pop();
+                if (op.equals("/") && second == 0)
+                    calcBoardLabel.setText("Error");
+                else
+                    numbers.push(applyOp(first, second, op));
+            }
+            if (!numbers.isEmpty()) {
+                double total = numbers.pop();
+                calcBoardLabel.setText(String.valueOf(total));
+                expression = String.valueOf(total);
+            }
+        }
+    }
+
+    private boolean hasPrecedence(String op1, String op2) {
+        if (op2.equals("(") || op2.equals(")")) {
+            return false;
+        } else if ((op1.equals("*") || op1.equals("/")) && (op2.equals("+") || op2.equals("-"))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private double applyOp(double a, double b, String op) {
+        switch (op) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b == 0) {
+                    throw new IllegalArgumentException("Cannot divide by zero");
+                }
+                return a / b;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + op);
+        }
     }
 }
