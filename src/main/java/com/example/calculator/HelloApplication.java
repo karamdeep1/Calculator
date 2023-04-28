59a97e629b50dc99d69bfcf0111570beabcffd91
Karamdeep <ricardo036942@gmail.com>
2023-04-28 13:52:12 -0400

    code commented and finished

--- src/main/java/com/example/calculator/HelloApplication.java
+++ src/main/java/com/example/calculator/HelloApplication.java
@@ -10,14 +10,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // loads fxml file
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        // creates a scene later to be put onto the stage
         Scene scene = new Scene(fxmlLoader.load(), 321, 290);
+        // gets rid of resizing the stage
         stage.setResizable(false);
-        stage.setTitle("Hello!");
+        // sets the title to calculator
+        stage.setTitle("Calculator");
+        // puts the scene onto the stage
         stage.setScene(scene);
+        //shows the stage
         stage.show();
     }
-
+    //main method to launch the application
     public static void main(String[] args) {
         launch();
     }
